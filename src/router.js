d269e12863c103ec81635dacb0f46e8f66fe2715
Ed Lomonaco <eman1986@users.noreply.github.com>
2017-09-04 19:05:43 -0400

    took out an unneeded component

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import CreateUser from './components/CreateUser.vue';
 import UserDetails from './components/UserDetails.vue';
-import UserList from './components/UserList.vue';
+import UserTable from './components/UserTable.vue';
 import UpdateUser from './components/UpdateUser.vue';
 import Vue from 'vue';
 import VueRouter from 'vue-router';
@@ -11,7 +11,7 @@ export default new VueRouter({
     saveScrollPosition: true,
     linkActiveClass: 'is-active',
     routes: [
-        { path: '/', name: 'Home', component: UserList },
+        { path: '/', name: 'Home', component: UserTable },
         { path: '/create', name: 'Create', component: CreateUser },
         { path: '/details/:id', name: 'Details', component: UserDetails },
         { path: '/update/:id', name: 'Edit', component: UpdateUser }
